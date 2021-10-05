0ff28a8884750f3d647c2d4929e8f2a0eec0ad8c
Lars Assen <juniorassen@hotmail.com>
2021-10-05 17:02:19 +0200

    added a meta component

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,9 +1,11 @@
 import React from 'react';
 import styles from '../../styles/Layout.module.scss';
 import NavBar from '../navagation/NavBar';
+import Meta from '../Meta/Meta';
 const Layout: React.FC = ({children}) => {
     return (
         <div>
+            <Meta />
             <NavBar />
         <div className={styles.container}>
             <main className={styles.main}>
