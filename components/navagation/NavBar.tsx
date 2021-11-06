5d0f860acf8809cd2a65fd4d1286f78309b0a63c
LarsAssen <juniorassen@hotmail.com>
2021-11-06 17:39:23 +0100

    added styling to the single blog page

--- components/navagation/NavBar.tsx
+++ components/navagation/NavBar.tsx
@@ -8,9 +8,9 @@ const NavBar: React.FC = () => {
 				<div className="flex justify-between">
 					<div className="flex space-x-7">
             <ul className="hidden md:flex items-center space-x-1">
-                <li className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"><Link href="/"><a>Home</a></Link></li>
-                <li className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"><Link href="/blog/Blog"><a>Blog</a></Link></li>
-                <li className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 transition duration-300"><Link href="/about/About"><a>About</a></Link></li>
+                <li className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 focus:ring-2 focus:ring-blue-600 transition duration-300"><Link href="/"><a>Home</a></Link></li>
+                <li className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 focus:ring-2 focus:ring-blue-600 duration-300"><Link href="/blog/Blog"><a>Blog</a></Link></li>
+                <li className="py-4 px-2 text-gray-500 font-semibold hover:text-green-500 focus:ring-2 focus:ring-blue-600 duration-300"><Link href="/about/About"><a>About</a></Link></li>
             </ul>
             </div>
             </div>
--- pages/post/[slug].tsx
+++ pages/post/[slug].tsx
@@ -2,7 +2,7 @@ import ReactMarkdown from "react-markdown";
 import Moment from "react-moment";
 import { fetchAPI } from "../../lib/api";
 import { getStrapiMedia } from "../../lib/media";
-
+import Link from 'next/link';
 
   type Post = {
       title: string
@@ -16,22 +16,25 @@ const Post: React.FC<{post: Post}> = ({post}) => {
   const imageUrl = getStrapiMedia(post.image)
 
     return (
-      <div>
+      <div className= "container w-full md:max-w-3xl mx-auto pt-20">
+        <div className="w-full px-4 md:px-6 text-xl text-gray-800 leading-normal">
         <div data-src={imageUrl} data-src-set={imageUrl} data-uk-img>
-          <h1>{post.title}</h1>
+        <p className="text-base md:text-sm text-green-500 font-bold">&lt; <Link href="/"><a className="text-base md:text-sm text-green-500 font-bold no-underline hover:underline">Back to blog</a></Link></p>
+          <h1 className="pt-4 pb-4">{post.title}</h1>
+          <p className="text-sm md:text-base font-normal text-gray-600">Published <Moment format="MMM Do YYYY">{post.published_at}</Moment></p>
           <img src={imageUrl} alt="" />
         </div>
         <div>
-          <div>
+          <div className="pt-6">
             <ReactMarkdown>{post.content}</ReactMarkdown>
             <div>
               <div>
                 <p>By Lars Assen</p>
-                <p><Moment format="MMM Do YYYY">{post.published_at}</Moment></p>
               </div>
             </div>
           </div>
 
+        </div>
         </div>
       </div>
     )
