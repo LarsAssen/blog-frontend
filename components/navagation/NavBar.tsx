32da4548c2b5b5850850befd31d4a02c6221fd1b
LarsAssen <juniorassen@hotmail.com>
2021-11-13 09:16:10 +0100

    fixed the active link a bit, but now somehow small errors keep popping up, might have to do with the rerender of the page

--- components/navagation/NavBar.tsx
+++ components/navagation/NavBar.tsx
@@ -1,7 +1,37 @@
-import React from 'react'
+import React, { useState } from 'react'
 import Link from 'next/link';
 
+const items = [ 
+    {
+        title: 'Home',
+        link: "/"
+    },
+    {
+        title: 'Blog',
+        link: "blog/Blog"
+    },
+    {
+        title: 'About',
+        link: "about/About"
+    }
+]
+
 const NavBar: React.FC = () => {
+    const [activeNav, setActiveNav] = useState<number>(1);
+
+    const onNavClick = (index: number) => {
+        setActiveNav(index)
+    }
+
+    const renderedNavs = items.map((item, index) =>{
+        const active = index === activeNav ? 'border-b-4 border-purple-500' : '';
+
+        return (<React.Fragment key={item.title}>
+            <li className={"py-4 px-2 text-gray-500 font-semibold " + active} onClick={() => onNavClick(index)} ><Link href={item.link}><a>{item.title}</a></Link></li>
+        </React.Fragment>)
+        
+    })
+
     return (
         <nav className="bg-white shadow-lg">
             <div className="max-w-6xl mx-auto px-4">
@@ -13,9 +43,7 @@ const NavBar: React.FC = () => {
                             </span>
                         </a>
                         <ul className="hidden md:flex items-center space-x-1">
-                            <li className="py-4 px-2 text-gray-500 border-b-4 active:bg-gray-100 border-purple-500 font-semibold"><Link href="/"><a>Home</a></Link></li>
-                            <li className="py-4 px-2 text-gray-500 border-b-4 active:bg-gray-100 border-purple-500 font-semibold"><Link href="/blog/Blog"><a>Blog</a></Link></li>
-                            <li className="py-4 px-2 text-gray-500 border-b-4 active:bg-gray-100 border-purple-500 font-semibold"><Link href="/about/About"><a>About</a></Link></li>
+                            {renderedNavs}
                         </ul>
                     </div>
                 </div>
