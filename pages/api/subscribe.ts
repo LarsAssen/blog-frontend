d2f2bb1a7c4e6e47c1ccc565b362d897c5785ead
LarsAssen <juniorassen@hotmail.com>
2021-11-19 11:06:02 +0100

    fixed some errors in subscribe file

--- pages/api/subscribe.ts
+++ pages/api/subscribe.ts
@@ -5,21 +5,23 @@ mailchimp.setConfig({
   server: process.env.MAILCHIMP_API_SERVER
 });
 
-export default async (req, res) => {
-    const { email } = req.body;
-  
-    if (!email) {
-      return res.status(400).json({ error: 'Email is required' });
-    }
-  
-    try {
-      await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
-        email_address: email,
-        status: 'subscribed'
-      });
-  
-      return res.status(201).json({ error: '' });
-    } catch (error) {
-      return res.status(500).json({ error: error.message || error.toString() });
-    }
-  };
+const subscribe = async (req: any, res: any) => {
+  const { email }:any = req.body;
+
+  if (!email) {
+    return res.status(400).json({ error: 'Email is required' });
+  }
+
+  try {
+    await mailchimp.lists.addListMember(process.env.MAILCHIMP_AUDIENCE_ID, {
+      email_address: email,
+      status: 'subscribed'
+    });
+
+    return res.status(201).json({ error: '' });
+  } catch (error:any) {
+    return res.status(500).json({ error: error.message || error.toString() });
+  }
+};
+
+export default subscribe;
