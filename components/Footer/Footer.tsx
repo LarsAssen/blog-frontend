f641f761c61f6dbb16929b5079a3e63369ee3cd5
LarsAssen <juniorassen@hotmail.com>
2021-11-24 17:24:52 +0100

    added a new style for the footer to the page

--- components/Footer/Footer.tsx
+++ components/Footer/Footer.tsx
@@ -2,27 +2,109 @@ import React from 'react'
 
 const Footer:React.FC = () => {
     return (
-<section className="bg-white py-8 w-full">
-    <div className="container mx-auto px-8">
-        <div className="table w-full">
-            
-            <div className="block sm:table-cell">
-                <p className="uppercase text-grey text-sm sm:mb-6">Social</p>
-                <ul className="list-reset text-xs mb-6">
-                    <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="https://www.facebook.com/lars.assen.9" className="text-grey hover:text-grey-dark">Facebook</a>
-                    </li>
-                    <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="https://www.linkedin.com/in/lars-assen/" className="text-grey hover:text-grey-dark">Linkedin</a>
-                    </li>
-                    <li className="mt-2 inline-block mr-2 sm:block sm:mr-0">
-                        <a href="https://github.com/LarsAssen" className="text-grey hover:text-grey-dark">GitHub</a>
-                    </li>
-                </ul>
-            </div>
+<footer className="border-t border-gray-200">
+      <div
+        className="
+          container
+          flex flex-wrap
+          items-center
+          justify-center
+          px-4
+          py-8
+          mx-auto
+          lg:justify-between
+        "
+      >
+        <div className="flex flex-wrap justify-center">
+          <ul className="flex items-center space-x-4">
+            <li>Home</li>
+            <li>Blog</li>
+            <li>About</li>
+          </ul>
         </div>
-    </div>
-</section>
+        <div className="flex items-center my-4 lg:my-0">
+          <form className="flex flex-wrap items-center justify-center">
+            <div className="mb-2 lg:mb-0 lg:mr-2">
+              <input
+                type="text"
+                name="newsletter"
+                className="
+                  w-full
+                  px-3
+                  py-2
+                  text-gray-700
+                  bg-gray-100
+                  border border-gray-300
+                  rounded
+                  outline-none
+                  focus:ring-1
+                "
+              />
+            </div>
+            <button
+              className="
+                px-6
+                py-2
+                text-white
+                bg-blue-500
+                border-0
+                rounded
+                focus:outline-none
+                hover:bg-blue-700
+              "
+            >
+              Subscribe
+            </button>
+          </form>
+        </div>
+        <div className="flex justify-center mt-4 lg:mt-0">
+          <a href="https://www.facebook.com/lars.assen.9/">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-6 h-6 text-blue-600"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M18 2h-3a5 5 0 00-5 5v3H7v4h3v8h4v-8h3l1-4h-4V7a1 1 0 011-1h3z"
+              ></path>
+            </svg>
+          </a>
+          <a href="https://twitter.com/LarsAssen" className="ml-3">
+            <svg
+              fill="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-6 h-6 text-blue-300"
+              viewBox="0 0 24 24"
+            >
+              <path
+                d="M23 3a10.9 10.9 0 01-3.14 1.53 4.48 4.48 0 00-7.86 3v1A10.66 10.66 0 013 4s-4 9 5 13a11.64 11.64 0 01-7 2c9 5 20 0 20-11.5a4.5 4.5 0 00-.08-.83A7.72 7.72 0 0023 3z"
+              ></path>
+            </svg>
+          </a>
+          <a href="https://www.instagram.com/larsassen/" className="ml-3">
+            <svg
+              fill="none"
+              stroke="currentColor"
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              strokeWidth="2"
+              className="w-6 h-6 text-pink-400"
+              viewBox="0 0 24 24"
+            >
+              <rect width="20" height="20" x="2" y="2" rx="5" ry="5"></rect>
+              <path
+                d="M16 11.37A4 4 0 1112.63 8 4 4 0 0116 11.37zm1.5-4.87h.01"
+              ></path>
+            </svg>
+          </a>
+        </div>
+      </div>
+    </footer>
     )
 }
 
