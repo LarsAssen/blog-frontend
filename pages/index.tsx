7b6d5157df36a9ce86c7e8fba944e4c8649a8ec9
LarsAssen <juniorassen@hotmail.com>
2021-11-25 20:24:20 +0100

    added a simple pagination, but still buggy with buttons not quite working correctly

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,9 @@
 import EmailSubscribe from '@/components/EmailSubscription/EmailSubscribe';
 import PostList from '@/components/Post/PostList';
 import { fetchAPI } from 'lib/api';
+import { useState } from 'react';
 import Header from '../components/Header/Header';
+import Pagination from '@/components/Post/Pagination';
 import Layout from '../components/Layout/Layout';
 
 type ImageType ={
@@ -29,11 +31,30 @@ export async function getStaticProps(){
 }
 
 const Home:React.FC<{articles:Post[], categories:any, homepage:any}> = ({articles, categories, homepage}) => {
+  const [currentPage, setCurrentPage] = useState(1);
+  const [postsPerPage] = useState(3);
+
+    // Get current posts
+    const indexOfLastPost = currentPage * postsPerPage;
+    const indexOfFirstPost = indexOfLastPost - postsPerPage;
+    const currentPosts = articles.slice(indexOfFirstPost, indexOfLastPost);
+  
+    // Change page
+    const paginateFront = () => setCurrentPage(currentPage + 1);
+    const paginateBack = () => setCurrentPage(currentPage - 1);
+  
   return (
     <div>
       <Header />
       <h2 className="text-3xl text-indigo-500">Latest posts</h2>
-      <PostList posts={articles}/>
+      <PostList posts={currentPosts}/>
+      <Pagination
+        postsPerPage={postsPerPage}
+        totalPosts={articles.length}
+        paginateBack={paginateBack}
+        paginateFront={paginateFront}
+        currentPage={currentPage}
+      />
     </div>
   )
 }
