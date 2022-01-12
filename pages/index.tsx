2e6b0f725ff6959322b859e78eee7af66eddbe63
LarsAssen <juniorassen@hotmail.com>
2022-01-12 16:38:17 +0100

    made the array only get first 3 posts

--- pages/index.tsx
+++ pages/index.tsx
@@ -21,12 +21,14 @@ const Home: React.FC<{ articles: Post[]; categories: any; homepage: any }> = ({
   categories,
   homepage,
 }) => {
+
+  const posts = articles.slice(0, 3);
   return (
     <div>
       <Header />
       <div className="container">
         <h2 className="text-3xl ml-8 text-blue-500">Latest posts</h2>
-        <PostList posts={articles} />
+        <PostList posts={posts} />
         <SubscribeBox />
       </div>
     </div>
