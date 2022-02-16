106678cdab303238b86b72a9b19c7cb5fec2a499
LarsAssen <juniorassen@hotmail.com>
2022-02-16 12:50:47 +0100

    transition attempt 1

--- components/Layout/Layout.tsx
+++ components/Layout/Layout.tsx
@@ -1,33 +1,58 @@
-import React from 'react';
+import React, { useState } from 'react';
 import NavBar from '../navagation/NavBar';
 import Meta from '../Meta/Meta';
 import Footer from '../Footer/Footer';
-import { motion } from 'framer-motion';
-const Layout: React.FC = ({children}) => {
+import { AnimatePresence, domAnimation, LazyMotion, m, motion } from 'framer-motion';
+import router from 'next/router';
+import { animations } from 'lib/animations';
+import AnimSwitcher from '../AnimSwitcher';
 
+
+const Layout: React.FC = ({children}) => {
+    const startIndex = 0;
+    const [animation, setAnimation] = useState(animations[startIndex]);
+    const [exitBefore, setExitBefore] = useState(false);
     const variants = {
         hidden: { opacity: 0, x: -200, y: 0 },
         enter: { opacity: 1, x: 0, y: 0 },
         exit: { opacity: 0, x: 0, y: -100 },
     }
 
+    const items = [ 
+        "/",
+        "/blog/Blog",
+        "/about/About",
+        '/about/CurrentLife/CurrentLifePage'
+    ]
+
     return (
         <div>
             <Meta title="The Running Explorer" keywords="ultrarunning, books, habits, travel" description="Everything about ultra running, health and self improvement. Learn all about how to become your best self." />
+            <div>
             <NavBar/>
             <div>
-                <main>
-                <motion.main
-                    variants={variants} // Pass the variant object into Framer Motion 
-                    initial="hidden" // Set the initial state to variants.hidden
-                    animate="enter" // Animated state to variants.enter
-                    exit="exit" // Exit state (used later) to variants.exit
-                    transition={{ type: 'linear' }} // Set the transition to linear
-                    className=""
+                <AnimSwitcher
+                    anims={animations}
+                    setAnimation={setAnimation}
+                    setExitBeforeEnter={setExitBefore}
+                    startIndex={startIndex}
+                />
+            </div>
+            <LazyMotion features={domAnimation}>
+                <AnimatePresence exitBeforeEnter={!exitBefore} >
+                <m.div
+                    key={items.concat(animation.name)}
+                    className="page-wrap"
+                    initial="initial"
+                    animate="animate"
+                    exit="exit"
+                    variants={animation.variants}
+                    transition={animation.transition}
                 >
-                    {children}
-                </motion.main>
-                </main>
+                            {children}
+                        </m.div>
+                </AnimatePresence>
+            </LazyMotion>
             </div>
             <Footer />
         </div>
