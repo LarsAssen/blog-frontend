4b00b74fdb43831b942c7604fb00b3bd9469ed1e
LarsAssen <juniorassen@hotmail.com>
2022-02-21 13:37:48 +0100

    fixed the image

--- components/navagation/NavBar.tsx
+++ components/navagation/NavBar.tsx
@@ -2,7 +2,7 @@ import React, { useState } from 'react'
 import Link from 'next/link';
 import { motion } from 'framer-motion';
 import Image from 'next/image';
-import Logo from '../../public/logo.jpg';
+import Logo from '../../public/logo.png';
 
 const items = [ 
     {
