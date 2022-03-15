653b612c5f1fa9e6b19d8bbd29dc524c9558415d
LarsAssen <juniorassen@hotmail.com>
2022-03-15 14:32:41 +0100

    footer Restructure

---
+++ components/Footer/FooterNav.tsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+const FooterNav:React.FC = () => {
+  return (
+    <ul className="flex items-center space-x-4">
+        <li>Home</li>
+        <li>Blog</li>
+        <li>About</li>
+    </ul>
+  )
+}
+
+export default FooterNav
