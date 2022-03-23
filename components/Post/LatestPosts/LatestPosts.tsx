432cbdb6bb501b8ebe0a28aa309ccfb29a8a684e
LarsAssen <juniorassen@hotmail.com>
2022-03-23 20:17:35 +0100

    latest posts rework

---
+++ components/Post/LatestPosts/LatestPosts.tsx
@@ -0,0 +1,16 @@
+import { Post } from 'Models/PostModel';
+import React from 'react';
+import PostItem from '../PostItem';
+
+
+const LatestPosts: React.FC<{ posts: Post[]}> = ({posts}) => {
+    return (
+    <div className="p-10 grid grid-cols-1 sm:grid-cols-1 md:grid-cols-1 lg:grid-cols-1 xl:grid-cols-3 gap-5">
+        {posts.map((post) => {
+            return <PostItem post={post} key={`article__${post.slug}`} />
+    })}
+    </div>
+    )
+}
+
+export default LatestPosts;
