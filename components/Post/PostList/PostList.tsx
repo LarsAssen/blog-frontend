a59146cbaf707be0a9cf815895670638356fa706
LarsAssen <juniorassen@hotmail.com>
2022-03-25 11:38:23 +0100

    fixed blog page

---
+++ components/Post/PostList/PostList.tsx
@@ -0,0 +1,20 @@
+import { Post } from 'Models/PostModel';
+import React from 'react';
+import PostItem from '../PostItem/PostItemBig';
+
+const PostList: React.FC<{ posts: Post[]}> = ({posts}) => {
+    return (
+        <div className="text-gray-600 body-font overflow-hidden">
+          <div className='container px-5 py-24 mx-auto'>
+            <h1 className="pb-5 mb-7 mx-auto">Blog</h1>
+            <div className='-my-8 divide-y-2 divide-gray-100'>
+              {posts.map((post) => {
+                return <PostItem post={post} key={`article__${post.slug}`} />
+              })}
+            </div>
+          </div>
+        </div>
+    )
+}
+
+export default PostList;
