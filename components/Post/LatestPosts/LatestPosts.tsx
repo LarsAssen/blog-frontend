c85f2dc40487e1d7fa7e6a3456936e9da418f004
LarsAssen <juniorassen@hotmail.com>
2022-03-30 14:36:13 +0200

    first attempt to animations

--- components/Post/LatestPosts/LatestPosts.tsx
+++ components/Post/LatestPosts/LatestPosts.tsx
@@ -1,18 +1,30 @@
+import { motion } from 'framer-motion';
 import { Post } from 'Models/PostModel';
 import React from 'react';
 import PostItem from '../PostItem/PostItemSmall';
 
+const container = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
 
 const LatestPosts: React.FC<{ posts: Post[]}> = ({posts}) => {
     return (
     <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
         <h2 className="text-3xl mb-7 text-blue-500">Latest posts</h2>
-            <div className="flex flex-auto flex-wrap -m-4">
+            <motion.div className="flex flex-auto flex-wrap -m-4" variants={container} initial="hidden" animate="visible">
                 {posts.map((post) => {
                     return <PostItem post={post} key={`article__${post.slug}`} />
                 })}
-            </div>
+            </motion.div>
         </div>
     </section>
     )
--- components/UI/CardSmall.tsx
+++ components/UI/CardSmall.tsx
@@ -1,14 +1,23 @@
+import { motion } from 'framer-motion'
 import React from 'react'
+
+const item = {
+  hidden: { y: 20, opacity: 0 },
+  visible: {
+    y: 0,
+    opacity: 1
+  }
+};
 
 const CardSmall:React.FC<{children: any}> = ({children}) => {
   return (
-    <div className="p-4 md:w-1/3">
+    <motion.div initial="hidden" animate="visible" variants={item} className="p-4 md:w-1/3">
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
               {children}
           </div>
         </div>
-    </div>
+    </motion.div>
   )
 }
 
