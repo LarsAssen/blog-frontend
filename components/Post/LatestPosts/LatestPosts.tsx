e8a5b92c97cc13995d239b7beacd12df4e0c8bac
LarsAssen <juniorassen@hotmail.com>
2022-04-07 13:50:08 +0200

    animations

--- components/Post/LatestPosts/LatestPosts.tsx
+++ components/Post/LatestPosts/LatestPosts.tsx
@@ -3,7 +3,7 @@ import { Post } from 'Models/PostModel';
 import React from 'react';
 import PostItem from '../PostItem/PostItemSmall';
 
-const container = {
+const list = {
     hidden: { opacity: 1, scale: 0 },
     visible: {
       opacity: 1,
@@ -15,14 +15,22 @@ const container = {
     }
   };
 
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+
 const LatestPosts: React.FC<{ posts: Post[]}> = ({posts}) => {
     return (
     <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
         <h2 className="text-3xl mb-7 text-blue-500">Latest posts</h2>
-            <motion.div className="flex flex-auto flex-wrap -m-4" variants={container} initial="hidden" animate="visible">
-                {posts.map((post) => {
-                    return <PostItem post={post} key={`article__${post.slug}`} />
+            <motion.div className="flex flex-auto flex-wrap -m-4 list" variants={list} initial="hidden" animate="visible">
+                {posts.map((post, index) => {
+                    return <PostItem className="item" variant={item} post={post} key={index} />
                 })}
             </motion.div>
         </div>
--- components/UI/CardSmall.tsx
+++ components/UI/CardSmall.tsx
@@ -1,17 +1,9 @@
 import { motion } from 'framer-motion'
 import React from 'react'
 
-const item = {
-  hidden: { y: 20, opacity: 0 },
-  visible: {
-    y: 0,
-    opacity: 1
-  }
-};
-
-const CardSmall:React.FC<{children: any}> = ({children}) => {
+const CardSmall:React.FC<{children: any, variant:any, className:string}> = ({children, variant, className}) => {
   return (
-    <motion.div initial="hidden" animate="visible" variants={item} className="p-4 md:w-1/3">
+    <motion.div initial="hidden" animate="visible" variants={variant} className={`p-4 md:w-1/3 ${className}`}>
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
               {children}
