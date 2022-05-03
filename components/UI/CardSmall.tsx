493d4bdad30d4724dafa0a135c5aa998981ac465
LarsAssen <juniorassen@hotmail.com>
2022-05-03 15:14:38 +0200

    fixed animation, but play only at reload

--- components/UI/CardSmall.tsx
+++ components/UI/CardSmall.tsx
@@ -3,7 +3,7 @@ import React from 'react'
 
 const CardSmall:React.FC<{children: any, variant:any, className:string}> = ({children, variant, className}) => {
   return (
-    <motion.div initial="hidden" animate="visible" variants={variant} className={`p-4 md:w-1/3 ${className}`}>
+    <motion.div initial="hidden" animate="visible" variants={variant} className={`p-4 ${className}`}>
         <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
           <div className="h-full border-2 border-gray-200 border-opacity-60 rounded-lg overflow-hidden">
               {children}
