96b50af212e0ebb5dbf35aa24a556c228cde603a
LarsAssen <juniorassen@hotmail.com>
2022-05-30 15:22:56 +0200

    fixed about page

--- components/UI/Title/TitleBig.tsx
+++ components/UI/Title/TitleBig.tsx
@@ -2,10 +2,10 @@ import React from 'react'
 
 const TitleBig:React.FC<{titleText:string}> = ({titleText}) => {
   return (
-    <div className='w-1/5 mb-5'>
-        <h1 className='text-black font-bold'>{titleText}</h1>
-        <div className='d inline-block w-1/2 border-b-4 border-solid border-main-color'></div>
-    </div>
+    <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+    <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-gray-900">{titleText}</h1>
+    <div className="h-1 w-20 bg-main-color rounded"></div>
+  </div>
   )
 }
 
