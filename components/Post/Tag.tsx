696e20031790e85f5be0ccc531329c3518a20c86
LarsAssen <juniorassen@hotmail.com>
2022-06-01 13:48:38 +0200

    fixed the tag component

--- components/Post/Tag.tsx
+++ components/Post/Tag.tsx
@@ -1,9 +1,11 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome'
+import { faTags } from '@fortawesome/free-solid-svg-icons'
 import React from 'react'
 
 const Tag:React.FC<{tagName:string}> = ({tagName}) => {
     return (
         <div>
-            <span className="inline-block bg-gray-200 rounded-full mx-3 px-3 py-1 text-sm font-semibold text-gray-700 mr-2 mb-2">{tagName}</span>
+            <span className="inline-block bg-main-color text-white rounded-full mx-3 px-3 py-1 text-sm font-semibold mr-2 mb-2"><FontAwesomeIcon icon={faTags} /> {tagName}</span>
         </div>
     )
 }
