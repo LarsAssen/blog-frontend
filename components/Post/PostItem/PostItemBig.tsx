b64c5bdc4aca94bdab0618c6230e3927232a761c
LarsAssen <juniorassen@hotmail.com>
2022-06-06 09:15:41 +0200

    big card fixes

--- components/Post/PostItem/PostItemBig.tsx
+++ components/Post/PostItem/PostItemBig.tsx
@@ -9,16 +9,11 @@ const PostItemBig: React.FC<{ post: any }> = ({ post }) => {
   return (
     <CardBig>
       <Image width={300} height={300} className="lg:h-48 md:h-36 w-full object-cover object-center" src={post.attributes.Image.data.attributes.url} alt={post.attributes.Image.data.attributes.alternativeText} />
-      <div className="md:flex-grow">
+      <div className="md:flex-grow ml-3">
         <TitleSmall titleText={post.attributes.Title} />
           <p className="leading-relaxed">{post.attributes.Description}</p>
           <Link href="/post/[slug]" as={`/post/${post.attributes.Slug}`}>
-          <a className="text-indigo-500 inline-flex items-center mt-4">Learn More
-            <svg className="w-4 h-4 ml-2" viewBox="0 0 24 24" stroke="currentColor" strokeWidth="2" fill="none" strokeLinecap="round" strokeLinejoin="round">
-              <path d="M5 12h14"></path>
-              <path d="M12 5l7 7-7 7"></path>
-            </svg>
-          </a>
+          <a className="text-indigo-500 inline-flex items-center mt-4">Learn More</a>
           </Link>
         </div>
     </CardBig>
--- components/UI/CardBig.tsx
+++ components/UI/CardBig.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardBig:React.FC<{children: any}> = ({children}) => {
   return (
-    <div className='py-8 my-3 bg-white flex border rounded-lg shadow-lg flex-wrap md:flex-nowrap'>
+    <div className='my-3 bg-white flex border rounded-lg overflow-hidden shadow-lg flex-wrap md:flex-nowrap'>
         {children}
     </div>
   )
