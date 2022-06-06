9987f1c5a62574f26bf8509f55d37e2a31e63077
LarsAssen <juniorassen@hotmail.com>
2022-06-06 16:11:07 +0200

    fixed the blog post page

--- components/Post/PostItem/PostItemBig.tsx
+++ components/Post/PostItem/PostItemBig.tsx
@@ -4,17 +4,34 @@ import Link from 'next/link'
 import React from 'react'
 import Image from 'next/image'
 import TitleSmall from '@/components/UI/Title/TitleSmall'
+import PostCategory from '../PostCategory'
+import Tag from '../Tag'
 
 const PostItemBig: React.FC<{ post: any }> = ({ post }) => {
   return (
     <CardBig>
-      <Image width={300} height={300} className="lg:h-48 md:h-36 w-full object-cover object-center" src={post.attributes.Image.data.attributes.url} alt={post.attributes.Image.data.attributes.alternativeText} />
-      <div className="md:flex-grow ml-3">
-        <TitleSmall titleText={post.attributes.Title} />
+      <div className='h-full w-96 relative'>
+        <Image layout='fixed' width={320} height={320} className='w-full h-full relative object-cover' src={post.attributes.Image.data.attributes.url} alt={post.attributes.Image.data.attributes.alternativeText} />
+      </div>
+        <div className="md:flex-grow ml-3 mt-3">
+          <div className='mb-2'>
+            <PostCategory categoryName={post.attributes.category.data.attributes.CategoryName} />
+          </div>
+          <TitleSmall titleText={post.attributes.Title} />
           <p className="leading-relaxed">{post.attributes.Description}</p>
-          <Link href="/post/[slug]" as={`/post/${post.attributes.Slug}`}>
-          <a className="text-indigo-500 inline-flex items-center mt-4">Learn More</a>
-          </Link>
+          <div className='mt-10'>
+            <Link href="/post/[slug]" as={`/post/${post.attributes.Slug}`}>
+            <a className="text-main-color inline-flex items-center mt-4">Learn More</a>
+            </Link>
+            <div className="flex float-right flex-row mt-6 mb-2">
+              {post.attributes.tags.data.map((tag: any) => {
+                return (
+                  <Tag key={tag.id} tagName={tag.attributes.TagName} />
+                );
+              })}
+            </div>
+          </div>
+          
         </div>
     </CardBig>
   )
--- components/UI/CardBig.tsx
+++ components/UI/CardBig.tsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 const CardBig:React.FC<{children: any}> = ({children}) => {
   return (
-    <div className='my-3 bg-white flex border rounded-lg overflow-hidden shadow-lg flex-wrap md:flex-nowrap'>
+    <div className='my-3 h-80 flex border rounded-lg overflow-hidden shadow-lg flex-wrap md:flex-nowrap'>
         {children}
     </div>
   )
