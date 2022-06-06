8c38be7611dee79fa5ba6e2e91b13796780e9b49
LarsAssen <juniorassen@hotmail.com>
2022-06-06 16:24:02 +0200

    post item big fix

--- components/Post/PostItem/PostItemBig.tsx
+++ components/Post/PostItem/PostItemBig.tsx
@@ -19,16 +19,20 @@ const PostItemBig: React.FC<{ post: any }> = ({ post }) => {
           </div>
           <TitleSmall titleText={post.attributes.Title} />
           <p className="leading-relaxed">{post.attributes.Description}</p>
-          <div className='mt-10'>
+          <div className='h-1/3 relative'>
+            <div className='absolute bottom-1 left-1'>
             <Link href="/post/[slug]" as={`/post/${post.attributes.Slug}`}>
             <a className="text-main-color inline-flex items-center mt-4">Learn More</a>
             </Link>
-            <div className="flex float-right flex-row mt-6 mb-2">
+            </div>
+            <div className="absolute bottom-1 right-1">
+            <div className="flex float-right flex-row">
               {post.attributes.tags.data.map((tag: any) => {
                 return (
                   <Tag key={tag.id} tagName={tag.attributes.TagName} />
                 );
               })}
+            </div>
             </div>
           </div>
           
--- components/Post/Tag.tsx
+++ components/Post/Tag.tsx
@@ -5,7 +5,7 @@ import React from 'react'
 const Tag:React.FC<{tagName:string}> = ({tagName}) => {
     return (
         <div>
-            <span className="inline-block bg-main-color text-white rounded-full mx-3 px-3 py-1 text-sm font-semibold mr-2 mb-2"><FontAwesomeIcon icon={faTags} /> {tagName}</span>
+            <span className="inline-block bg-main-color text-white rounded-full mx-3 px-3 py-1 text-sm font-semibold mr-2"><FontAwesomeIcon icon={faTags} /> {tagName}</span>
         </div>
     )
 }
