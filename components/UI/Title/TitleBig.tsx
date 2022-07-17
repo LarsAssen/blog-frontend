1396cd88294a500eaf9831271e449fedfabf291d
LarsAssen <juniorassen@hotmail.com>
2022-07-17 19:32:02 +0200

    custom sized title

--- components/UI/Title/TitleBig.tsx
+++ components/UI/Title/TitleBig.tsx
@@ -1,12 +1,25 @@
 import React from 'react'
 
-const TitleBig:React.FC<{titleText:string}> = ({titleText}) => {
+const TitleBig:React.FC<{titleText:string, variant: string | null}> = ({titleText, variant="normal"}) => {
+
+  if(variant === "normal"){
+    return (
+    <div className="w-full mb-6 lg:mb-0">
+      <h1 className="text-lg font-bold title-font mb-2 text-gray-900">{titleText}</h1>
+      <div className="h-1 w-20 bg-main-color rounded"></div>
+    </div>
+    )
+  }
+
+
   return (
     <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
     <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-gray-900">{titleText}</h1>
     <div className="h-1 w-20 bg-main-color rounded"></div>
   </div>
   )
+
+
 }
 
 export default TitleBig
