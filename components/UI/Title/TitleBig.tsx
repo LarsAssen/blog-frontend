fe512d8a7b0116fcd41d39db5e5f52cfef85e290
LarsAssen <juniorassen@hotmail.com>
2022-07-18 19:47:32 +0200

    added large to title

--- components/UI/Title/TitleBig.tsx
+++ components/UI/Title/TitleBig.tsx
@@ -11,6 +11,14 @@ const TitleBig:React.FC<{titleText:string, variant: string | null}> = ({titleTex
     )
   }
 
+  if(variant === "large"){
+  return (
+    <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
+    <h1 className="sm:text-3xl text-2xl font-bold title-font mb-2 text-gray-900">{titleText}</h1>
+    <div className="h-1 w-20 bg-main-color rounded"></div>
+  </div>
+  )
+  }
 
   return (
     <div className="lg:w-1/2 w-full mb-6 lg:mb-0">
@@ -18,8 +26,6 @@ const TitleBig:React.FC<{titleText:string, variant: string | null}> = ({titleTex
     <div className="h-1 w-20 bg-main-color rounded"></div>
   </div>
   )
-
-
 }
 
 export default TitleBig
