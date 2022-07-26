9e11f518324c2e49ebfe8b115ed3f5bb7d5a9bf4
LarsAssen <juniorassen@hotmail.com>
2022-07-26 11:25:50 +0200

    titleBig Fix

--- components/Post/LatestPosts/LatestPosts.tsx
+++ components/Post/LatestPosts/LatestPosts.tsx
@@ -28,8 +28,7 @@ const LatestPosts: React.FC<{ posts: Post[]}> = ({posts}) => {
     return (
     <section className="text-gray-600 body-font">
         <div className="container px-5 py-24 mx-auto">
-        {/* <h2 className="text-3xl mb-7 text-blue-500">Latest posts</h2> */}
-        <TitleBig titleText='Latest posts' />
+        <TitleBig variant="normal" titleText='Latest posts' />
             <motion.div className="flex flex-auto flex-wrap mt-5 -m-4 list" variants={list} initial="hidden" animate="visible">
                 {posts.map((post, index) => {
                     return <motion.div className="item md:w-1/3" variants={item} key={index}><PostItem className="item" variant={item} post={post} key={index} /></motion.div>
