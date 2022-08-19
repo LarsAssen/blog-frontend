63c528e5c233c846b32718f0e7254e9d54e17527
LarsAssen <juniorassen@hotmail.com>
2022-08-19 15:55:03 +0200

    built animations in postlist

--- components/Post/PostList/PostList.tsx
+++ components/Post/PostList/PostList.tsx
@@ -6,6 +6,7 @@ import PostItem from '../PostItem/PostItem';
 import { getPostsPerPage } from 'services/postServices/postService';
 import Category from 'Models/CategoryModel';
 import Size from 'Models/Enums/Size';
+import { motion } from 'framer-motion';
 
 const PostList: React.FC<{ posts: Post[], categories:Category[], totalPosts:number}> = ({posts, categories, totalPosts}) => {
   const [currentPosts, setCurrentPosts] = React.useState(posts);
@@ -39,16 +40,37 @@ const PostList: React.FC<{ posts: Post[], categories:Category[], totalPosts:numb
     const nextPosts = await getPostsPerPage(page, postsPerPage);
     setCurrentPosts(nextPosts);
   }
+
+  const list = {
+    hidden: { opacity: 1, scale: 0 },
+    visible: {
+      opacity: 1,
+      scale: 1,
+      transition: {
+        delayChildren: 0.3,
+        staggerChildren: 0.2
+      }
+    }
+  };
+
+  const item = {
+    hidden: { y: 20, opacity: 0 },
+    visible: {
+      y: 0,
+      opacity: 1
+    }
+  };
+
     return (
         <div className="text-gray-600 body-font overflow-hidden">
           <div className='container px-5 py-24 mx-auto'>
             <Title size={Size.Large} text="Blog" />
             <Pagination paginateFront={getNextPage} paginateBack={getPreviousPage} totalPosts={totalPosts} currentPage={currentPage} postsPerPage={postsPerPage} />
-            <div className='flex flex-auto flex-wrap mt-5 -m-4 list'>
+            <motion.div variants={list} initial="hidden" animate="visible" className='flex flex-auto flex-wrap mt-5 -m-4 list'>
               {currentPosts.map((post) => {
-                return <PostItem variant={""} className="item md:w-1/3" size={Size.Medium} post={post} key={`article__${post.slug}`} />
+                return <motion.div variants={item} className="item md:w-1/3"  key={`article__${post.slug}`}><PostItem variant={""} className="" size={Size.Medium} post={post} key={`article__${post.slug}`} /></motion.div>
               })}
-            </div>
+            </motion.div>
             <Pagination paginateFront={getNextPage} paginateBack={getPreviousPage} totalPosts={totalPosts} currentPage={currentPage} postsPerPage={postsPerPage} />
           </div>
         </div>
